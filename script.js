187514c68c7a7cbf5ee2b5ddd2f89e7f195eecb7
alvaro <pedro_alvaro@outlook.com.pe>
2020-06-25 23:34:05 -0500

    propagacion de los eventos solucion

--- script.js
+++ script.js
@@ -1,46 +1,6 @@
-
-
-const video = document.getElementById('video'),
-    play  = document.getElementById('play'),
-    pause= document.getElementById('pause')
-
-
-play.addEventListener('click',()=>{
-    video.play()
-})
-pause.addEventListener('click',()=>{
-    video.pause()
-})
-
-
-
-/* addEventListener('scroll',e=>{
-    console.log(scrollX,scrollY)
-})
-addEventListener('resize',e=>{
-    console.log(innerWidth,innerHeight)
-    console.log(outerWidth,outerHeight)
-})
- */
-
-
-/* addEventListener('DOMContentLoaded',()=>{
-    const formulario=document.getElementById('formu')
-    const recordar=document.getElementById('recordar')
-    formulario.addEventListener('submit',()=>{
-        console.log('he enviado el formulario')
-    })
-    
-    
-    recordar.addEventListener('change',e=>{
-        if (e.target.checked) {
-            console.log('el usuario quiere recordar su contraseña')
-        }
-        else{
-            console.log('el usuario no quiere recordar sus contraseña')
-        }
+document.querySelectorAll('div').forEach(el =>{
+    el.addEventListener('click',e=>{
+        console.log(`click en ${e.target.id}`)
+        e.stopPropagation()
     })
 })
-
- */
-
