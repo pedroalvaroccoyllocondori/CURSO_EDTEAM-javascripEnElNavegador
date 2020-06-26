d24df90f6880f7df91ade3356ba7d035569966e3
alvaro <pedro_alvaro@outlook.com.pe>
2020-06-25 21:21:01 -0500

    eventos del  formulario y dom content loader

--- script.js
+++ script.js
@@ -1,150 +1,25 @@
 
 
 
-let x=0, y=0
-
-addEventListener('keyup',e=>{
 
 
-const pelota= document.getElementById('ball')
-
-
-const movimiento=direccion=>{
-    switch (direccion) {
-        case 'arriba':
-            y--    
-        break;
-        case 'abajo':
-            y++    
-        break;
-        case 'derecha':
-            x++    
-        break;
-        case 'izquierda':
-            x--    
-        break;
+addEventListener('DOMContentLoaded',()=>{
+    const formulario=document.getElementById('formu')
+    const recordar=document.getElementById('recordar')
+    formulario.addEventListener('submit',()=>{
+        console.log('he enviado el formulario')
+    })
     
-        default:
-            break;
-    }
-    pelota.style.transform=`translate(${x*10}px,${y*10}px)`
-}
-switch (e.key) {
-        case 'ArrowUp':
-            movimiento('arriba')
-            break;
-        case 'ArrowDown':
-            movimiento('abajo')
-            break;
-        case 'ArrowLeft':
-            movimiento('izquierda')
-            break;
-        case 'ArrowRight':
-            movimiento('derecha')
-            break;
     
-        default:
-            break;
-}
+    recordar.addEventListener('change',e=>{
+        if (e.target.checked) {
+            console.log('el usuario quiere recordar su contraseña')
+        }
+        else{
+            console.log('el usuario no quiere recordar sus contraseña')
+        }
+    })
 })
 
 
-/* 
-function hola(){
-    alert('hola  mundo')
-}
-
-const titulo= document.querySelector('h1')
-
-console.log(titulo.textContent)
-console.log(titulo.innerHTML)
-console.log(titulo.outerHTML)
-
-
-
-
-
-
- */
-
-
-/* const titulos =document.getElementById('oa')
-
-const holamundo=e=>console.log(e.target.textContent)
-const menucontextual=()=>{
-    const menu=document.createElement('div')
-    menu.textContent='hola  spy un menu'
-    document.body.appendChild(menu)
-
-    menu.style.padding='1em'
-    menu.style.background='yellow'
-    menu.style.position='fixed'
-    menu.style.top=`${e.pageY}px`
-    menu.style.left=`${e.pageX}px`
-
-}
-titulos.addEventListener('contextmenu',e=>{
-    menucontextual(e)
-    console.log(e.pageX,e.pageY)
-    e.preventDefault()
-})
-
- */
-
-
-
-
-/* titulo.textContent='hola a todos'
-titulo.innerHTML='hola a todos  <em>mis amigos</em>'
-
-////////////////////////////////////
-const profesor = document.createElement('h2')
-profesor.textContent='josue zavala'
-profesor.classList.add('teachhher')
-profesor.id='teacherjavascript'
-/////////////////////////////////////////////////
-const profesorcaja=document.getElementById('teacher')
-
-if (profesorcaja) {
-    profesorcaja.querySelector('span').appendChild(profesor)
-    
-}
-
-document.querySelector('button').addEventListener('click',holamundo)
-
-
-const holamundo=e=>console.log(e.target.textContent)
-profesor.addEventListener('dblclick',holamundo)
- */
-
-
-
-
-
-/* const nodo=document.querySelectorAll('li')
-for(let el of nodo){
-
-    if(el.textContent.trim().toUpperCase()==='FLORI'){
-        el.remove()
-    }
-} */
-
-
-
-
-
-
-
-
-
-/* 
-
-const nodo=Array.from(document.querySelectorAll('li'))
-
-nodo.map(el=>{
-    if(el.textContent.trim().toUpperCase()==='NINA'){
-        el.remove()
-    }
-})
- */
 
