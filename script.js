1ac2216ff4e493905519001bb982e2e1a10c779d
alvaro <pedro_alvaro@outlook.com.pe>
2020-06-26 11:30:15 -0500

    delegacion de eventos con objetos dinamicos

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.querySelectorAll('div').forEach(el =>{
+/* document.querySelectorAll('div').forEach(el =>{
     el.addEventListener('click',e=>{
         console.log(`click en ${e.target.id}`)
         e.stopPropagation()
     })
+}) */
+
+const galeria=document.getElementById('galeria')
+
+galeria.addEventListener('click',e=>{
+    const objetivo=e.target,
+          imagenes= Array.from(galeria.querySelectorAll('img'))
+    let numero=imagenes.indexOf(objetivo)
+    console.log(`imagem ${numero+1}`)
 })
+
+
