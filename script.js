47106d33692baf8b6c3a89745eec72b8eba1849c
alvaro <pedro_alvaro@outlook.com.pe>
2020-06-28 18:25:54 -0500

    insert adjent (insercionde  hermanos e hijos)

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const post = document.getElementById('post')
 
-const postparrafo= post.querySelectorAll('p')
+const padre = document.getElementById('padre')
 
-const anuncio = document.createElement('div')
+const elemento = document.createElement('div')
+elemento.id='nuevo'
+elemento.textContent='soy el nuevo elemto'
+
+//padre.insertAdjacentElement('beforebegin',elemento)//insercion hermananterior
+//padre.insertAdjacentElement('afterend',elemento)// insercion hermana posterior
+
+//padre.insertAdjacentElement('afterbegin',elemento)/// insercion hijo anterior
+
+//padre.insertAdjacentElement('beforeend',elemento)/// insercion hijo anterior
+
+//padre.insertAdjacentText('afterbegin','hola mundo')// insercion de texto
+
+///estos meetodos insert tex funciona con texto  tambn
+
+///************************* */metodo para insertar heml
+padre.insertAdjacentHTML('beforebegin',`<h1>soy el titulo</h1>`)
 
 
-anuncio.id='insertar'
-anuncio.textContent='publicidad'
-
-
-const  obtennermitadhijos=elemento=>{
-    const hijos=elemento.children
-    const longitud=hijos.length
-    const mitad = Math.floor(longitud/2)
-
-    return hijos[mitad]
-}
-// obtener la mitad de los elemtos
-console.log(obtennermitadhijos(post))
-
-const hijomedio=obtennermitadhijos(post)
-
-post.insertBefore(anuncio,hijomedio)
