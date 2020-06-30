23818907969bff67c4d8bd9cfd6fa37e8daf5b5e
alvaro <pedro_alvaro@outlook.com.pe>
2020-06-30 01:26:06 -0500

    cssom (dom del css)

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-const  plantilla= document.getElementById('plantilla')
+const titulo= document.querySelector('h1')
+const descripcion= document.querySelector('p')
 
-const miplatillanueva= plantilla.content.cloneNode(true)
-miplatillanueva.querySelector('h2').textContent='edteam'
-miplatillanueva.querySelector('p').textContent='tu futuro te esta esperando'
+titulo.style.color='blue'
+titulo.style.backgroundColor='aqua'
 
-document.getElementById('contenido').appendChild(miplatillanueva)
+const mediabp= matchMedia('(max-width: 640px) and (orientation:portrait)')
+console.log(mediabp.matches)
+
+const cambiarcolor=()=>{
+    if (mediabp.matches) {
+        document.body.style.background='red'
+    }else{
+        document.body.style.background='aqua'
+    }
+
+}
+addEventListener('resize',cambiarcolor)// se aplica cuando cambia el tamaño
+addEventListener('DOMContentLoaded',cambiarcolor)// se aplica cuando se carga el dom
+
+console.log(getComputedStyle(titulo).fontSize)
+console.log(getComputedStyle(document.body).fontSize)
+console.log(getComputedStyle(document.body).position)
+console.log(getComputedStyle(document.body).display)//valores asignados por defecto
+
+
+//$0.getBoundingClientRect() da las propiedades de valor del elemto tamañoa lato largo .etc
+///del archivo dibujado en el dom
+
+
+//$0.naturalHeight//altura del archivo real
+//$0.naturalWidth// ancho del archivo real
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* //metodo sin match media
+if (window.innerWidth>640) {
+    document.body.style.backgroundColor='yellow'
+} */
