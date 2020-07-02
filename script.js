e478e9c61262087da2c75df2c9932a6347c16f2c
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-01 23:43:39 -0500

    ejercicio relog con timers

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
 
-
-let contador=0
-
-setTimeout(()=>{
-    alert('hola mundo')
-},3000)///tiempo en milisegundos
-
-//varias acciones
-
-const saludarmuchasveces=setInterval(()=>{
-    contador++
-    if (contador>1) {
-        console.log(`hola ${contador} veces`)
-        
+setInterval(()=>{
+    const relog=document.getElementById('relog')
+    if (relog) {
+        relog.textContent=new Date().toLocaleTimeString()
     }
-    else{
-        console.log(`hola ${contador} vez`)
 
-    }
-    if(contador==2){
-        clearInterval(saludarmuchasveces)
-
-    }
-},1000)///tiempo en milisegundos
-
-console.log(new Date().toLocaleString())
-console.log(new Date().toString())
-
+},1000)
