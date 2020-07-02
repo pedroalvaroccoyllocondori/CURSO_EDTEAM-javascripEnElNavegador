b600194863135284002f917ff702623e7f816230
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-01 23:34:08 -0500

    objeto timers

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-const formu= document.getElementById('formulario')
-console.log(formu)
-if (formu) {
-    formu.addEventListener('submit',() => {
-        let ancho= formu.querySelector('#ancho')
-        let alto= formu.querySelector('#alto')
-        let url= formu.querySelector('#url')
-
-        if (ancho && alto && url ) {
-            ancho=ancho.value
-            alto=alto.value
-            url=url.value
-            
-        }
-        window.open(url,url,`innerWidth=${ancho} ,innerHeight=${alto}`)
 
 
-    })
-}
+let contador=0
+
+setTimeout(()=>{
+    alert('hola mundo')
+},3000)///tiempo en milisegundos
+
+//varias acciones
+
+const saludarmuchasveces=setInterval(()=>{
+    contador++
+    if (contador>1) {
+        console.log(`hola ${contador} veces`)
+        
+    }
+    else{
+        console.log(`hola ${contador} vez`)
+
+    }
+    if(contador==2){
+        clearInterval(saludarmuchasveces)
+
+    }
+},1000)///tiempo en milisegundos
+
+console.log(new Date().toLocaleString())
+console.log(new Date().toString())
+
